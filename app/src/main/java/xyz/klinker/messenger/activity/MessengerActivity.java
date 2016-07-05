987242013c1d32d505e86adc10b9bc66f91c163f
Jake Klinker <jklinker1@gmail.com>
2016-07-05 14:54:01 -0500

    set up initial drawer ui

--- app/src/main/java/xyz/klinker/messenger/activity/MessengerActivity.java
+++ app/src/main/java/xyz/klinker/messenger/activity/MessengerActivity.java
@@ -17,17 +17,166 @@
 package xyz.klinker.messenger.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import xyz.klinker.messenger.R;
 
 /**
  * Main entry point to the app. This will serve for setting up the drawer view, finding
  * conversations and displaying things on the screen to get the user started.
  */
-public class MessengerActivity extends AppCompatActivity {
+public class MessengerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_messenger);
+        initToolbar();
+        initDrawer();
+        initFab();
+    }
+
+    private void initToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        if (actionBar != null && drawerLayout != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void initDrawer() {
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        clickDefaultDrawerItem();
+    }
+
+    private void initFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        closeDrawer();
+
+        if (item.isChecked()) {
+            return true;
+        }
+
+        if (item.isCheckable()) {
+            item.setChecked(true);
+        }
+
+        if (drawerLayout != null) {
+            if (item.getItemId() == R.id.drawer_conversation) {
+                setTitle(R.string.app_title);
+            } else if (item.isCheckable()) {
+                setTitle(item.getTitle());
+            }
+        }
+
+        switch (item.getItemId()) {
+            case R.id.drawer_conversation:  return displayConversations();
+            case R.id.drawer_schedule:      return displayScheduledMessages();
+            case R.id.drawer_contacts:      return displayContacts();
+            case R.id.drawer_invite:        return displayInviteFriends();
+            case R.id.drawer_settings:      return displaySettings();
+            case R.id.drawer_help:          return displayHelp();
+            case R.id.drawer_about:         return displayAbout();
+            default:                        return true;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_messenger, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                openDrawer();
+                return true;
+            case R.id.menu_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void clickDefaultDrawerItem() {
+        clickNavigationItem(R.id.drawer_conversation);
+    }
+
+    private void clickNavigationItem(int itemId) {
+        onNavigationItemSelected(navigationView.getMenu().findItem(itemId));
+    }
+
+    private void openDrawer() {
+        if (drawerLayout != null) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+    }
+
+    private void closeDrawer() {
+        if (drawerLayout != null) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    private boolean displayConversations() {
+        return true;
+    }
+
+    private boolean displayScheduledMessages() {
+        return true;
+    }
+
+    private boolean displayContacts() {
+        return true;
+    }
+
+    private boolean displayInviteFriends() {
+        return true;
+    }
+
+    private boolean displaySettings() {
+        return true;
+    }
+
+    private boolean displayHelp() {
+        return true;
+    }
+
+    private boolean displayAbout() {
+        return true;
     }
 
 }
