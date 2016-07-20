470ec38f385b0440439958d84d504aee8dac1cf9
Jake Klinker <jklinker1@gmail.com>
2016-07-20 14:31:20 -0500

    change aspect ratio of image capture fragment to fill the whole width

--- app/src/main/java/xyz/klinker/messenger/view/AutoFitTextureView.java
+++ app/src/main/java/xyz/klinker/messenger/view/AutoFitTextureView.java
@@ -66,9 +66,9 @@ public class AutoFitTextureView extends TextureView {
             setMeasuredDimension(width, height);
         } else {
             if (width < height * mRatioWidth / mRatioHeight) {
-                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
-            } else {
                 setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            } else {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
             }
         }
     }
