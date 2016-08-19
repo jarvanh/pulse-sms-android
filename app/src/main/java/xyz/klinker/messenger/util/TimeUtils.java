b1a10b4b7d0329220a18ad0d4d13dc07b35f8229
Jake Klinker <jklinker1@gmail.com>
2016-08-18 19:07:00 -0500

    don't force close with timestamps being off, some devices just have different times

--- app/src/main/java/xyz/klinker/messenger/util/TimeUtils.java
+++ app/src/main/java/xyz/klinker/messenger/util/TimeUtils.java
@@ -46,10 +46,6 @@ public class TimeUtils {
      * @return true if we should display the timestamp, false otherwise.
      */
     public static boolean shouldDisplayTimestamp(long timestamp, long nextTimestamp) {
-        if (nextTimestamp < timestamp - 5000) {
-            throw new RuntimeException("nextTimestamp must be larger than timestamp");
-        }
-
         return nextTimestamp >= timestamp + (15 * MINUTE);
     }
 
