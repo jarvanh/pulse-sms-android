07916cd9d06b7895fc1c7932e86ac742f8ca7c26
Luke Klinker <lklinker1@gmail.com>
2016-08-28 17:51:59 -0500

    add missing file

---
+++ app/src/main/java/xyz/klinker/messenger/util/UpdateUtils.java
@@ -0,0 +1,14 @@
+package xyz.klinker.messenger.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class UpdateUtils {
+    public static void checkForUpdate(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        // i just want to hold this value, in case we need it later when we roll out public.
+        sharedPreferences.edit().putBoolean("is_on_pre_release", true).commit();
+    }
+}
