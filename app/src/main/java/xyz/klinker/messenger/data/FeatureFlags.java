9471ea7236001ffed65f1c57c561e4ecb19a058c
Luke Klinker <lklinker1@gmail.com>
2016-09-16 18:07:09 -0500

    add feature flag for messaging style notifications

--- app/src/main/java/xyz/klinker/messenger/data/FeatureFlags.java
+++ app/src/main/java/xyz/klinker/messenger/data/FeatureFlags.java
@@ -2,8 +2,10 @@ package xyz.klinker.messenger.data;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.annotation.VisibleForTesting;
+import android.support.compat.BuildConfig;
 
 /**
  * We can use these for new features or if we want to test something quick and don't know if it
@@ -27,17 +29,25 @@ public class FeatureFlags {
     }
     //endregion
 
-    // I saw some people online trimming their data before the decryption
+    // ADDING FEATURE FLAGS:
+    // 1. Add the static identifiers and flag name right below here.
+    // 2. Set up the flag in the constructor
+    // 3. Add the switch case for the flag in the updateFlag method
+
+
     private static final String FLAG_TRIM_DECRYPTION = "flag_trim_decryption";
-    private static final boolean TRIM_DECRYPTION_DEFAULT = false;
     public boolean TRIM_DECRYPTION;
+
+    private static final String FLAG_MESSAGING_STYLE_NOTIFICATIONS = "flag_messaging_notifications";
+    public boolean MESSAGING_STYLE_NOTIFICATIONS;
 
     private Context context;
     private FeatureFlags(final Context context) {
         this.context = context;
         SharedPreferences sharedPrefs = getSharedPrefs();
 
-        TRIM_DECRYPTION = sharedPrefs.getBoolean(FLAG_TRIM_DECRYPTION, TRIM_DECRYPTION_DEFAULT);
+        TRIM_DECRYPTION = sharedPrefs.getBoolean(FLAG_TRIM_DECRYPTION, getDefault());
+        MESSAGING_STYLE_NOTIFICATIONS = sharedPrefs.getBoolean(FLAG_MESSAGING_STYLE_NOTIFICATIONS, getDefault());
     }
 
     public void updateFlag(String identifier, boolean flag) {
@@ -49,10 +59,17 @@ public class FeatureFlags {
             case FLAG_TRIM_DECRYPTION:
                 TRIM_DECRYPTION = flag;
                 break;
+            case FLAG_MESSAGING_STYLE_NOTIFICATIONS:
+                MESSAGING_STYLE_NOTIFICATIONS = flag;
+                break;
         }
     }
 
     private SharedPreferences getSharedPrefs() {
         return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    private boolean getDefault() {
+        return BuildConfig.DEBUG;
     }
 }
