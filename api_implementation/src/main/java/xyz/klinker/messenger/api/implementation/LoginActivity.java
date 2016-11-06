3b5c5512972227d25bb52f0a5e3c229b7260353c
Luke Klinker <lklinker1@gmail.com>
2016-11-06 06:32:04 -0600

    add a 'forgot password' button to the login, that will take them to the forgot password FAQ

--- api_implementation/src/main/java/xyz/klinker/messenger/api/implementation/LoginActivity.java
+++ api_implementation/src/main/java/xyz/klinker/messenger/api/implementation/LoginActivity.java
@@ -25,6 +25,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -75,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText name;
     private EditText phoneNumber;
     private TextView errorHint;
+    private View forgotPassword;
     private ProgressDialog dialog;
 
     @Override
@@ -162,6 +164,7 @@ public class LoginActivity extends AppCompatActivity {
         fab = (FloatingActionButton) findViewById(R.id.login_fab);
         email = (EditText) findViewById(R.id.login_email);
         password = (EditText) findViewById(R.id.login_password);
+        forgotPassword = findViewById(R.id.forgot_password);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -183,6 +186,15 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
                 return handled;
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://messenger.klinkerapps.com/faq.html#forgot_password"));
+                startActivity(browserIntent);
             }
         });
 
