07fa5bd8951d016234c3a5b99c51d27a151607b8
Luke Klinker <lklinker1@gmail.com>
2017-03-24 20:21:40 -0500

    improve deleting messages from the notification

--- shared/src/main/java/xyz/klinker/messenger/shared/service/NotificationDeleteService.java
+++ shared/src/main/java/xyz/klinker/messenger/shared/service/NotificationDeleteService.java
@@ -10,6 +10,7 @@ import java.util.List;
 import xyz.klinker.messenger.api.implementation.Account;
 import xyz.klinker.messenger.api.implementation.ApiUtils;
 import xyz.klinker.messenger.shared.data.DataSource;
+import xyz.klinker.messenger.shared.data.MimeType;
 import xyz.klinker.messenger.shared.data.model.Message;
 import xyz.klinker.messenger.shared.receiver.ConversationListUpdatedReceiver;
 import xyz.klinker.messenger.shared.util.UnreadBadger;
@@ -31,10 +32,18 @@ public class NotificationDeleteService extends IntentService {
         DataSource source = DataSource.getInstance(this);
         source.open();
         source.deleteMessage(messageId);
-        List<Message> messages = source.getMessages(conversationId, 10);
 
-        if (messages.size() == 0) {
+        List<Message> messages = source.getMessages(conversationId, 1);
+        Message latest = null;
+
+        if (messages.size() == 1) {
+            latest = messages.get(0);
+        }
+
+        if (latest == null) {
             source.deleteConversation(conversationId);
+        } else if (latest.mimeType.equals(MimeType.TEXT_PLAIN)) {
+            source.updateConversation(conversationId, true, latest.timestamp, latest.data, latest.mimeType, false);
         }
 
         // cancel the notification we just replied to or
@@ -53,7 +62,9 @@ public class NotificationDeleteService extends IntentService {
                 Account.get(this).deviceId,
                 conversationId);
 
-        ConversationListUpdatedReceiver.sendBroadcast(this, conversationId, "", true);
+        ConversationListUpdatedReceiver.sendBroadcast(this, conversationId,
+                (latest != null && latest.mimeType.equals(MimeType.TEXT_PLAIN)) ? latest.data : "",
+                true);
 
         new UnreadBadger(this).writeCountFromDatabase();
     }
