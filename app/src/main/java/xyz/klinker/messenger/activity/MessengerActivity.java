ecc46f261f0b95ebe22b30881aaeb8dc7478edee
Luke Klinker <lklinker1@gmail.com>
2017-07-12 09:37:19 -0500

    revert debugging change

--- app/src/main/java/xyz/klinker/messenger/activity/MessengerActivity.java
+++ app/src/main/java/xyz/klinker/messenger/activity/MessengerActivity.java
@@ -137,8 +137,6 @@ public class  MessengerActivity extends AppCompatActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toast.makeText(this, Build.MANUFACTURER + ": " + Build.MODEL, Toast.LENGTH_LONG).show();
-
         dataSource = DataSource.getInstance(this);
         dataSource.open();
 
