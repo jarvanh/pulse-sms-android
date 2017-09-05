81ab3c61f3e126f94024c1a7bd4df5db836c1c3f
Luke Klinker <lklinker1@gmail.com>
2017-09-05 06:44:09 -0500

    make sure we are finishing our job scheduler jobs

--- shared/src/main/java/xyz/klinker/messenger/shared/service/jobs/BackgroundJob.java
+++ shared/src/main/java/xyz/klinker/messenger/shared/service/jobs/BackgroundJob.java
@@ -1,5 +1,6 @@
 package xyz.klinker.messenger.shared.service.jobs;
 
+import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.os.AsyncTask;
@@ -9,12 +10,18 @@ public abstract class BackgroundJob extends JobService {
     abstract void onRunJob(JobParameters parameters);
 
     @Override
+    @SuppressLint("StaticFieldLeak")
     public final boolean onStartJob(final JobParameters params) {
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... args) {
                 onRunJob(params);
                 return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void v) {
+                jobFinished(params, false);
             }
         }.execute();
 
