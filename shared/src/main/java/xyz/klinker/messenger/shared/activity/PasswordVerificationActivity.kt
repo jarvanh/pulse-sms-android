cc0245161d328ca1d989727609c1b19be6508f07
Luke Klinker <lklinker1@gmail.com>
2018-04-17 08:41:54 -0500

    finish the passcode input view

--- shared/src/main/java/xyz/klinker/messenger/shared/activity/PasswordVerificationActivity.kt
+++ shared/src/main/java/xyz/klinker/messenger/shared/activity/PasswordVerificationActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -12,8 +13,15 @@ import xyz.klinker.android.floating_tutorial.FloatingTutorialActivity
 import xyz.klinker.android.floating_tutorial.TutorialPage
 import com.github.ajalt.reprint.core.AuthenticationFailureReason
 import com.github.ajalt.reprint.core.AuthenticationListener
+import com.raycoarana.codeinputview.CodeInputView
 import xyz.klinker.messenger.shared.R
 import xyz.klinker.messenger.shared.data.Settings
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.net.Uri
+import java.lang.reflect.AccessibleObject.setAccessible
+
+
 
 
 class PasswordVerificationActivity : FloatingTutorialActivity(), AuthenticationListener {
@@ -65,22 +73,51 @@ class FingerprintPage(context: FloatingTutorialActivity) : TutorialPage(context)
 @SuppressLint("ViewConstructor")
 class PasscodePage(context: FloatingTutorialActivity) : TutorialPage(context) {
 
-    private val nextButton: Button by lazy { getActivity().findViewById<View>(R.id.tutorial_next_button) as Button }
+    private val nextButton: Button by lazy { findViewById<View>(R.id.tutorial_next_button) as Button }
+    private val passcode: CodeInputView by lazy { findViewById<View>(R.id.code_input) as CodeInputView }
 
     override fun initPage() {
-        setContentView(R.layout.page_fingerprint)
-        setNextButtonText(R.string.verify)
+        setContentView(R.layout.page_passcode)
+        setNextButtonText(R.string.forgot_passcode)
+
+        val userPasscode = "1234" //Settings.privateConversationsPasscode
+
+        passcode.setShowKeyboard(true)
+        passcode.inPasswordMode = true
+
+        passcode.addOnCompleteListener { code ->
+            if (code == userPasscode) {
+                setActivityResult(Activity.RESULT_OK)
+                getActivity().finishAnimated()
+            } else {
+                passcode.setError(R.string.incorrect_passcode)
+                Handler().postDelayed({
+                    passcode.setEditable(true)
+                    passcode.clearError()
+                    passcode.setShowKeyboard(true)
+                    passcode.requestFocus()
+
+                    try {
+                        val method = passcode.javaClass.getDeclaredMethod("deleteCharacter")
+                        method.isAccessible = true
+                        method.invoke(passcode)
+                        method.invoke(passcode)
+                        method.invoke(passcode)
+                        method.invoke(passcode)
+                    } catch (e: Throwable) {
+                    }
+                }, 500)
+            }
+        }
+
+        nextButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://messenger.klinkerapps.com/forgot_passcode.html"))
+            getActivity().startActivity(browserIntent)
+        }
     }
 
     override fun onShown(firstTimeShown: Boolean) {
         super.onShown(firstTimeShown)
-
-        if (!firstTimeShown) {
-            return
-        }
-
-        nextButton.setOnClickListener {
-            setActivityResult(Activity.RESULT_OK)
-        }
+        passcode.requestFocus()
     }
 }
