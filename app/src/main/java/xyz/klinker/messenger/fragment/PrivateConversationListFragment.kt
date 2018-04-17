a23d39837697b0d81f041d62758e1f429102bc0f
Luke Klinker <lklinker1@gmail.com>
2018-04-17 11:45:53 -0500

    tell the user about the password protection on the private conversations page

--- app/src/main/java/xyz/klinker/messenger/fragment/PrivateConversationListFragment.kt
+++ app/src/main/java/xyz/klinker/messenger/fragment/PrivateConversationListFragment.kt
@@ -1,5 +1,7 @@
 package xyz.klinker.messenger.fragment
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v7.widget.helper.ItemTouchHelper
@@ -14,11 +16,36 @@ import xyz.klinker.messenger.adapter.conversation.ConversationListAdapter
 import xyz.klinker.messenger.adapter.view_holder.ConversationViewHolder
 import xyz.klinker.messenger.fragment.conversation.ConversationListFragment
 import xyz.klinker.messenger.shared.data.DataSource
+import xyz.klinker.messenger.shared.data.Settings
 import xyz.klinker.messenger.shared.data.model.Conversation
 import xyz.klinker.messenger.utils.swipe_to_dismiss.SwipeTouchHelper
 import xyz.klinker.messenger.utils.swipe_to_dismiss.UnarchiveSwipeSimpleCallback
 
 class PrivateConversationListFragment : ConversationListFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, viewGroup: ViewGroup?, bundle: Bundle?): View? {
+        val view = super.onCreateView(inflater, viewGroup, bundle)
+
+        val fragmentActivity = activity
+        if (fragmentActivity != null && Settings.privateConversationsPasscode.isNullOrBlank()) {
+            val prefs = Settings.getSharedPrefs(fragmentActivity)
+            if (prefs.getBoolean("private_conversation_security_disclainer", true)) {
+                AlertDialog.Builder(fragmentActivity)
+                        .setMessage(R.string.enable_passcode_disclaimer)
+                        .setPositiveButton(android.R.string.ok, { _, _ -> })
+                        .setNegativeButton(R.string.menu_feature_settings, { _, _ ->
+                            if (fragmentActivity is MessengerActivity) {
+                                fragmentActivity.clickNavigationItem(R.id.drawer_feature_settings)
+                            }
+                        }).show()
+
+                prefs.edit().putBoolean("private_conversation_security_disclainer", false).commit()
+            }
+        }
+
+
+        return view
+    }
 
     override fun noConversationsText() = getString(R.string.no_private_messages_description)
 
