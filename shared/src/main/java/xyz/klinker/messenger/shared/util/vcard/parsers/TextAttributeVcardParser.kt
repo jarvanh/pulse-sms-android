03875d135163fc90edf65f5fa70d6e5fee0993eb
Luke Klinker <lklinker1@gmail.com>
2018-04-26 09:28:20 -0500

    parse more types of text attributes from vcards (phone, email, url, etc)

--- shared/src/main/java/xyz/klinker/messenger/shared/util/vcard/parsers/TextAttributeVcardParser.kt
+++ shared/src/main/java/xyz/klinker/messenger/shared/util/vcard/parsers/TextAttributeVcardParser.kt
@@ -1,6 +1,7 @@
 package xyz.klinker.messenger.shared.util.vcard.parsers
 
 import android.content.Context
+import android.util.Log
 import xyz.klinker.messenger.shared.data.MimeType
 import xyz.klinker.messenger.shared.data.model.Message
 import xyz.klinker.messenger.shared.util.StringUtils
@@ -22,6 +23,8 @@ class TextAttributeVcardParser(context: Context) : VcardParser(context) {
         val cards = mutableListOf<MutableList<String>>()
 
         for (line in lines) {
+            Log.v("pulse_vcard", line)
+
             if (line.contains(START_LINE)) {
                 cards.add(mutableListOf())
             }
@@ -47,6 +50,8 @@ class TextAttributeVcardParser(context: Context) : VcardParser(context) {
         return when {
             line.contains(FULL_NAME) -> readFullName(line)
             line.contains(PHONE_NUMBER) -> readPhoneNumber(line)
+            line.contains(EMAIL) -> readEmail(line)
+            line.contains(URL) -> readUrl(line)
             else -> throw IllegalArgumentException("this line shouldn't have made it here: $line")
         }
     }
@@ -57,10 +62,67 @@ class TextAttributeVcardParser(context: Context) : VcardParser(context) {
 
     private fun readPhoneNumber(line: String): String {
         return try {
-            val type = line.substring(line.indexOf("=") + 1, line.indexOf(","))
-            val number = line.substring(line.indexOf("VOICE:") + "VOICE:".length)
+            when {
+                line.contains("TYPE=") -> { // Pulse's style of vCard
+                    val type = line.substring(line.indexOf("=") + 1, line.indexOf(","))
+                    val number = line.substring(line.indexOf("VOICE:") + "VOICE:".length)
+                    "${StringUtils.titleize(type)}: $number"
+                }
+                else -> {
+                    val line = line.replace("TEL;", "").replace("TEL:", "")
+                            .replace("PREF:", "").replace("PREF;", "")
 
-            "${StringUtils.titleize(type)}: $number"
+                    val type = when {
+                        line.contains(":") -> line.substring(0, line.indexOf(":"))
+                        line.contains(";") -> line.substring(0, line.indexOf(";"))
+                        else -> "Phone"
+                    }
+
+                    val number = when {
+                        line.contains("CELL") -> line.substring(line.indexOf("CELL") + "CELL:".length)
+                        line.contains("WORK") -> line.substring(line.indexOf("WORK") + "WORK:".length)
+                        line.contains("HOME") -> line.substring(line.indexOf("HOME") + "HOME:".length)
+                        line.contains("VOICE") -> line.substring(line.indexOf("VOICE") + "VOICE:".length)
+                        else -> throw IllegalArgumentException("can't parse this line: $line")
+                    }
+
+                    "${StringUtils.titleize(type)}: $number"
+                }
+            }
+
+        } catch (e: Exception) {
+            ""
+        }
+    }
+
+    private fun readEmail(line: String): String {
+        return try {
+            val line = line.replace("EMAIL;", "").replace("EMAIL:", "")
+                    .replace("PREF;", "").replace("PREF:", "")
+
+            val type = when {
+                line.contains(":") -> line.substring(0, line.indexOf(":"))
+                line.contains(";") -> line.substring(0, line.indexOf(";"))
+                else -> "Email"
+            }
+
+            val address = when {
+                line.contains("HOME") -> line.substring(line.indexOf("HOME") + "HOME:".length)
+                line.contains("WORK") -> line.substring(line.indexOf("WORK") + "WORK:".length)
+                !line.contains(":") -> line
+                else -> throw IllegalArgumentException("can't parse this line: $line")
+            }
+
+            "${StringUtils.titleize(type)}: $address"
+        } catch (e: Exception) {
+            ""
+        }
+    }
+
+    private fun readUrl(line: String): String {
+        return try {
+            val url = line.replace("URL:", "")
+            "URL: $url"
         } catch (e: Exception) {
             ""
         }
@@ -72,7 +134,9 @@ class TextAttributeVcardParser(context: Context) : VcardParser(context) {
 
         private const val FULL_NAME = "FN:"
         private const val PHONE_NUMBER = "TEL;"
+        private const val EMAIL = "EMAIL"
+        private const val URL = "URL:"
 
-        private val PARSABLE_LINES = listOf(FULL_NAME, PHONE_NUMBER)
+        private val PARSABLE_LINES = listOf(FULL_NAME, PHONE_NUMBER, EMAIL, URL)
     }
 }
