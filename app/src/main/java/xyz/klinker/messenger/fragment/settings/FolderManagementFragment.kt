67bc4a01daa8915adfbef9ddaf238b90da54f602
Luke Klinker <lklinker1@gmail.com>
2018-05-01 09:32:50 -0500

    add new auto replies from that settings page

--- app/src/main/java/xyz/klinker/messenger/fragment/settings/FolderManagementFragment.kt
+++ app/src/main/java/xyz/klinker/messenger/fragment/settings/FolderManagementFragment.kt
@@ -1,26 +1,29 @@
 package xyz.klinker.messenger.fragment.settings
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.preference.PreferenceCategory
-import android.support.v7.app.AppCompatDelegate
+import android.preference.Preference
+import android.preference.PreferenceGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
 
 import xyz.klinker.messenger.R
-import xyz.klinker.messenger.api.implementation.Account
-import xyz.klinker.messenger.api.implementation.ApiUtils
 import xyz.klinker.messenger.shared.data.ColorSet
-import xyz.klinker.messenger.shared.data.FeatureFlags
+import xyz.klinker.messenger.shared.data.DataSource
 import xyz.klinker.messenger.shared.data.Settings
-import xyz.klinker.messenger.shared.util.AndroidVersionUtil
-import xyz.klinker.messenger.shared.util.ColorUtils
+import xyz.klinker.messenger.shared.data.model.Folder
 import xyz.klinker.messenger.shared.util.DrawerItemHelper
-import xyz.klinker.messenger.shared.util.listener.ColorSelectedListener
-import xyz.klinker.messenger.view.ColorPreference
 
 class FolderManagementFragment : MaterialPreferenceFragment() {
+
+    private val foldersPrefGroup: PreferenceGroup by lazy { findPreference(getString(R.string.pref_folders_category)) as PreferenceGroup }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.settings_folder)
+
+        fillFolderList()
     }
 
     override fun onStop() {
@@ -28,4 +31,67 @@ class FolderManagementFragment : MaterialPreferenceFragment() {
         DrawerItemHelper.folders = null
     }
 
+
+    private fun fillFolderList() {
+        val createNewFolder = Preference(activity)
+        createNewFolder.setTitle(R.string.create_folder)
+        createNewFolder.setSummary(R.string.create_folder_summary)
+        createNewFolder.setOnPreferenceClickListener {
+            promptCreateNewFolder()
+            true
+        }
+
+        foldersPrefGroup.removeAll()
+        foldersPrefGroup.addPreference(createNewFolder)
+
+        DataSource.getFoldersAsList(activity)
+                .forEach {
+                    val pref = createPreference(it)
+                    foldersPrefGroup.addPreference(pref)
+                }
+    }
+
+    private fun promptCreateNewFolder() {
+        val layout = LayoutInflater.from(activity).inflate(R.layout.dialog_edit_text, null, false)
+        val editText = layout.findViewById<View>(R.id.edit_text) as EditText
+        editText.setHint(R.string.folder_name)
+
+        AlertDialog.Builder(activity)
+                .setView(layout)
+                .setPositiveButton(R.string.create) { _, _ ->
+                    val signature = editText.text.toString()
+                    createAndShowFolder(signature)
+                }
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
+    }
+
+    private fun promptEditFolder() {
+
+    }
+
+    private fun createAndShowFolder(title: String) {
+        val folder = Folder()
+        folder.id = DataSource.generateId()
+        folder.name = title
+        folder.colors = ColorSet.DEFAULT(activity)
+
+        DataSource.insertFolder(activity, folder, true)
+
+        val pref = createPreference(folder)
+        foldersPrefGroup.addPreference(pref)
+
+        DrawerItemHelper.folders = null
+    }
+
+    private fun createPreference(folder: Folder): Preference {
+        val pref = Preference(activity)
+        pref.title = folder.name
+
+        pref.setOnPreferenceClickListener {
+            true
+        }
+
+        return pref
+    }
 }
