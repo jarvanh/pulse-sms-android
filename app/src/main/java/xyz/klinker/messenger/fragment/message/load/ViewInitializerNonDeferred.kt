99421c9a12f49aee89d40cf4e53a3b0792efadea
Luke Klinker <lklinker1@gmail.com>
2018-05-02 06:45:14 -0500

    disable hardware acceleration on Android 8.0 on the message list edit text

--- app/src/main/java/xyz/klinker/messenger/fragment/message/load/ViewInitializerNonDeferred.kt
+++ app/src/main/java/xyz/klinker/messenger/fragment/message/load/ViewInitializerNonDeferred.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.Toolbar
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.support.design.widget.FloatingActionButton
@@ -71,6 +72,10 @@ class ViewInitializerNonDeferred(private val fragment: MessageListFragment) {
 
         if (bundle == null) {
             fragment.messageLoader.initRecycler()
+        }
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
+            messageEntry.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
         }
     }
 
