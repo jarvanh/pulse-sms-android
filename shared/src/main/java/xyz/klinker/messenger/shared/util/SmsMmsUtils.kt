2f03a94b0c2c43336d76eea8c9598119902cbcb7
Luke Klinker <lklinker1@gmail.com>
2018-06-25 19:23:15 -0500

    up the import limit for testing

--- shared/src/main/java/xyz/klinker/messenger/shared/util/SmsMmsUtils.kt
+++ shared/src/main/java/xyz/klinker/messenger/shared/util/SmsMmsUtils.kt
@@ -54,8 +54,8 @@ object SmsMmsUtils {
 
     private val TAG = "SmsMmsUtils"
 
-    val INITIAL_CONVERSATION_LIMIT = 250
-    val INITIAL_MESSAGE_LIMIT = 500
+    val INITIAL_CONVERSATION_LIMIT = 1000
+    val INITIAL_MESSAGE_LIMIT = 10000
 
     /**
      * Gets a list of conversations from the internal sms database that is ready to be inserted
