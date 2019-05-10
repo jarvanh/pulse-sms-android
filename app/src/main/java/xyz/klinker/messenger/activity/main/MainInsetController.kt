493f2a4763d8a04d692229d90d88167fa7188166
Luke Klinker <lklinker1@gmail.com>
2019-05-10 16:32:06 -0700

    update the layout for the scheduled messages

--- app/src/main/java/xyz/klinker/messenger/activity/main/MainInsetController.kt
+++ app/src/main/java/xyz/klinker/messenger/activity/main/MainInsetController.kt
@@ -2,11 +2,13 @@ package xyz.klinker.messenger.activity.main
 
 import android.annotation.SuppressLint
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
 import xyz.klinker.messenger.activity.MessengerActivity
+import xyz.klinker.messenger.fragment.ScheduledMessagesFragment
 import xyz.klinker.messenger.fragment.conversation.ConversationListFragment
 import xyz.klinker.messenger.fragment.message.MessageListFragment
 import xyz.klinker.messenger.shared.util.AndroidVersionUtil
@@ -56,6 +58,20 @@ class MainInsetController(private val activity: MessengerActivity) {
         val recycler = fragment.recyclerView
         recycler.clipToPadding = false
         recycler.setPadding(recycler.paddingLeft, recycler.paddingTop, recycler.paddingRight, recycler.paddingBottom + bottomInsetValue)
+    }
+
+    fun modifyScheduledMessageElements(fragment: ScheduledMessagesFragment) {
+        if (!AndroidVersionUtil.isAndroidQ) {
+            return
+        }
+
+        val recycler = fragment.list
+        recycler.clipToPadding = false
+        recycler.setPadding(recycler.paddingLeft, recycler.paddingTop, recycler.paddingRight, recycler.paddingBottom + bottomInsetValue)
+
+        // move fab above the nav bar
+        val params = fragment.fab.layoutParams as FrameLayout.LayoutParams
+        params.bottomMargin = sixteenDp + bottomInsetValue
     }
 
     fun modifyMessageListElements(fragment: MessageListFragment) {
