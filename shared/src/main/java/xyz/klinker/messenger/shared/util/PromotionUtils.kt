5d31db11e9a93b716fe012e38cd17e055270282c
Luke Klinker <lklinker1@gmail.com>
2020-08-03 09:47:32 -0500

    update the 'rate it' support

--- shared/src/main/java/xyz/klinker/messenger/shared/util/PromotionUtils.kt
+++ shared/src/main/java/xyz/klinker/messenger/shared/util/PromotionUtils.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Handler
+import com.sensortower.rating.RatingPrompt
+import com.sensortower.rating.RatingPromptOptions
 import xyz.klinker.messenger.api.implementation.Account
 import xyz.klinker.messenger.shared.activity.RateItDialog
 import xyz.klinker.messenger.shared.data.Settings
@@ -23,25 +25,21 @@ class PromotionUtils(private val context: Context) {
         return Account.exists() && Account.subscriptionType == Account.SubscriptionType.FREE_TRIAL && Account.getDaysLeftInTrial() <= 0
     }
 
+    // The "rate-it" library will manage whether or not it has been shown or whether it needs to be
+    // shown. This shared preference key is so that we are not re-prompting users that have seen
+    // the dialog in the past, through the legacy system.
+
     private fun shouldAskForRating(): Boolean {
-        val pref = "install_time"
-        val currentTime = TimeUtils.now
-        val installTime = sharedPreferences.getLong(pref, -1L)
-
-        if (installTime == -1L) {
-            // write the install time to now
-            sharedPreferences.edit().putLong(pref, currentTime).commit()
-        } else {
-            if (currentTime - installTime > TimeUtils.TWO_WEEKS) {
-                return sharedPreferences.getBoolean("show_rate_it", true)
-            }
-        }
-
-        return false
+        return sharedPreferences.getBoolean("show_rate_it", true)
     }
 
     private fun askForRating() {
-        sharedPreferences.edit().putBoolean("show_rate_it", false).commit()
-        Handler().postDelayed({ context.startActivity(Intent(context, RateItDialog::class.java)) }, 500)
+        Handler().postDelayed({
+            val options = RatingPromptOptions.Builder("Pulse")
+                    .useEmojis(false)
+                    .initialPromptTimeout(3)
+                    .build()
+            RatingPrompt.show(context, options)
+        }, 500)
     }
 }
