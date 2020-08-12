c722da08df4f552405e3ae723301cb05af6a7eaf
Luke Klinker <lklinker1@gmail.com>
2020-08-12 12:29:59 -0500

    update when the prompt to rate the app is shown

--- shared/src/main/java/xyz/klinker/messenger/shared/util/PromotionUtils.kt
+++ shared/src/main/java/xyz/klinker/messenger/shared/util/PromotionUtils.kt
@@ -10,12 +10,11 @@ import xyz.klinker.messenger.api.implementation.Account
 import xyz.klinker.messenger.shared.data.Settings
 
 class PromotionUtils(private val context: Activity) {
-    private val sharedPreferences: SharedPreferences = Settings.getSharedPrefs(context)
 
     fun checkPromotions(onTrialExpired: () -> Unit) {
         if (trialExpired()) {
             onTrialExpired()
-        } else if (shouldAskForRating()) {
+        } else {
             askForRating()
         }
     }
@@ -24,17 +23,13 @@ class PromotionUtils(private val context: Activity) {
         return Account.exists() && Account.subscriptionType == Account.SubscriptionType.FREE_TRIAL && Account.getDaysLeftInTrial() <= 0
     }
 
-    // The "rate-it" library will manage whether or not it has been shown or whether it needs to be
-    // shown. This shared preference key is so that we are not re-prompting users that have seen
-    // the dialog in the past, through the legacy system.
-
-    private fun shouldAskForRating(): Boolean {
-        return sharedPreferences.getBoolean("show_rate_it", true)
-    }
+    // The "rate-it" library will manage whether or not it has been shown or whether it needs to be shown.
+    // If the user has already rated the app, Google Play will manage this case.
 
     private fun askForRating() {
         Handler().postDelayed({
             RatingPrompt.show(context)
         }, 500)
     }
+    
 }
