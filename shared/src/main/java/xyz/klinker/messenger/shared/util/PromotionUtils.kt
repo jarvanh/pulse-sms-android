38ba4cf79851c4483e28de1bb1c43c82f5cf7a28
Luke Klinker <lklinker1@gmail.com>
2020-10-05 07:27:21 -0500

    update dependency and version number

--- shared/src/main/java/xyz/klinker/messenger/shared/util/PromotionUtils.kt
+++ shared/src/main/java/xyz/klinker/messenger/shared/util/PromotionUtils.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.os.Handler
 import com.sensortower.rating.RatingPrompt
 import com.sensortower.rating.RatingPromptOptions
-import com.sensortower.rating.RatingPromptSettings
+import com.sensortower.rating.util.RatingPromptSettings
 import xyz.klinker.messenger.api.implementation.Account
 import xyz.klinker.messenger.shared.data.Settings
 
@@ -26,16 +26,6 @@ class PromotionUtils(private val context: Activity) {
         if (Account.exists() && !Account.primary) {
             // only prompt for rating on the primary device
             return
-        }
-
-        val settings = RatingPromptSettings.getInstance(context)
-        val sharedPrefs = Settings.getSharedPrefs(context)
-
-        if (sharedPrefs.getBoolean("update_rating_method", true)) {
-            sharedPrefs.edit().putBoolean("update_rating_method", false).commit()
-
-            settings.putBoolean("rating-prompt-dont-show-again", false)
-            settings.putLong("rating-prompt-should-show-at-timestamp", -1L)
         }
 
         Handler().postDelayed({
